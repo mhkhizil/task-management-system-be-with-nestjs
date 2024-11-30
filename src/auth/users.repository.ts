5b5631fef41366829614b3a1c7ea4347ecad6bb2
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2024-12-01 00:33:01 +0630

    adding hashing functionality to pas

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,6 +1,8 @@
 import { Repository, DataSource } from 'typeorm';
 import { User } from './entities/user.entity';
 import { createAuthCredentialDto } from './dto/create-authCredential.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt'
 //custom repo usage default repo use yin ka module forfeatrure htl mhr entity ko define p service mhr @InjectRepository devorator tin p inject yone pl custom repo so yin constructor khan p datasource api ko use ya dl aoyyin module yk provider htl mhr pr twr declare ya dl
 export class UserRepository extends Repository<User> {
   constructor(private readonly dataSource: DataSource) {
@@ -10,7 +12,18 @@ export class UserRepository extends Repository<User> {
     createAuthCredentialDto: createAuthCredentialDto,
   ): Promise<void> {
     const { username, password } = createAuthCredentialDto;
-    const user = this.create({ username, password });
-    await this.save(user);
+    const salt= await bcrypt.genSalt();
+    const hashed=await bcrypt.hash(password,salt)
+    const user = this.create({ username, password:hashed });
+    try {
+            await this.save(user);
+    } catch (error) {
+        if (error.code ==='23505') {
+            throw new ConflictException('User already exists')
+        }else{
+            throw new InternalServerErrorException()
+        }
+    }
+
   }
 }
